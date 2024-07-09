e299b6dcbd274750625660aa17bc0a6d58c38175
NimrodBar <131991393+NimiB2@users.noreply.github.com>
2024-07-09 12:12:04 +0300

    table 1.0

--- src/main/java/dev/nimrod/customchart/CustomTableView.java
+++ src/main/java/dev/nimrod/customchart/CustomTableView.java
@@ -1,8 +1,10 @@
 package dev.nimrod.customchart;
 
 import android.content.Context;
+import android.graphics.Typeface;
 import android.util.AttributeSet;
 import android.view.LayoutInflater;
+import android.view.View;
 import android.widget.LinearLayout;
 import android.widget.TableLayout;
 import android.widget.TableRow;
@@ -12,7 +14,6 @@ import com.google.android.material.textview.MaterialTextView;
 
 public class CustomTableView extends LinearLayout {
     private boolean isRowNumberingEnabled = false;
-
     private TableLayout tableLayout;
     private MaterialTextView tableTitle;
 
@@ -78,7 +79,6 @@ public class CustomTableView extends LinearLayout {
         }
     }
 
-
     public void enableRowNumbering() {
         isRowNumberingEnabled = true;
         updateRowNumbers();
@@ -96,63 +96,201 @@ public class CustomTableView extends LinearLayout {
             numberCell.setText(String.valueOf(i + 1));
             numberCell.setPadding(8, 8, 8, 8);
             if (isRowNumberingEnabled) {
-                row.addView(numberCell, 0);
+                if (row.getChildCount() == 0 || !(row.getChildAt(0) instanceof TextView && ((TextView) row.getChildAt(0)).getText().toString().equals(String.valueOf(i + 1)))) {
+                    row.addView(numberCell, 0);
+                }
             } else {
-                row.removeViewAt(0);
+                if (row.getChildCount() > 0 && row.getChildAt(0) instanceof TextView && ((TextView) row.getChildAt(0)).getText().toString().equals(String.valueOf(i + 1))) {
+                    row.removeViewAt(0);
+                }
             }
         }
     }
+
     public void setCellColor(int row, int column, int color) {
-        TableRow tableRow = (TableRow) tableLayout.getChildAt(row);
-        TextView cell = (TextView) tableRow.getChildAt(column);
-        cell.setBackgroundColor(color);
-    }
-
-    public void setRowColor(int row, int color) {
-        TableRow tableRow = (TableRow) tableLayout.getChildAt(row);
-        for (int i = 0; i < tableRow.getChildCount(); i++) {
-            TextView cell = (TextView) tableRow.getChildAt(i);
-            cell.setBackgroundColor(color);
-        }
-    }
-
-    public void setColumnColor(int column, int color) {
-        for (int i = 0; i < tableLayout.getChildCount(); i++) {
-            TableRow tableRow = (TableRow) tableLayout.getChildAt(i);
+        if (isValidPosition(row, column)) {
+            TableRow tableRow = (TableRow) tableLayout.getChildAt(row);
             TextView cell = (TextView) tableRow.getChildAt(column);
             cell.setBackgroundColor(color);
         }
     }
+
+    public void setRowColor(int row, int color) {
+        if (isValidRow(row)) {
+            TableRow tableRow = (TableRow) tableLayout.getChildAt(row);
+            for (int i = 0; i < tableRow.getChildCount(); i++) {
+                TextView cell = (TextView) tableRow.getChildAt(i);
+                cell.setBackgroundColor(color);
+            }
+        }
+    }
+
+    public void setColumnColor(int column, int color) {
+        if (isValidColumn(column)) {
+            for (int i = 0; i < tableLayout.getChildCount(); i++) {
+                TableRow tableRow = (TableRow) tableLayout.getChildAt(i);
+                if (column < tableRow.getChildCount()) {
+                    TextView cell = (TextView) tableRow.getChildAt(column);
+                    cell.setBackgroundColor(color);
+                }
+            }
+        }
+    }
+
     public void setCellTextColor(int row, int column, int color) {
-        TableRow tableRow = (TableRow) tableLayout.getChildAt(row);
-        TextView cell = (TextView) tableRow.getChildAt(column);
-        cell.setTextColor(color);
+        if (isValidPosition(row, column)) {
+            TableRow tableRow = (TableRow) tableLayout.getChildAt(row);
+            TextView cell = (TextView) tableRow.getChildAt(column);
+            cell.setTextColor(color);
+        }
     }
 
     public void setCellTextSize(int row, int column, float size) {
-        TableRow tableRow = (TableRow) tableLayout.getChildAt(row);
-        TextView cell = (TextView) tableRow.getChildAt(column);
-        cell.setTextSize(size);
+        if (isValidPosition(row, column)) {
+            TableRow tableRow = (TableRow) tableLayout.getChildAt(row);
+            TextView cell = (TextView) tableRow.getChildAt(column);
+            cell.setTextSize(size);
+        }
     }
+
+    public void setCellTextStyle(int row, int column, int style) {
+        if (isValidPosition(row, column)) {
+            TableRow tableRow = (TableRow) tableLayout.getChildAt(row);
+            TextView cell = (TextView) tableRow.getChildAt(column);
+            cell.setTypeface(null, style);
+        }
+    }
+
     public void insertRow(int position, String[] cellValues) {
-        TableRow row = new TableRow(getContext());
-        for (String cellValue : cellValues) {
+        if (position >= 0 && position <= tableLayout.getChildCount()) {
+            TableRow row = new TableRow(getContext());
+            for (String cellValue : cellValues) {
+                TextView cell = new TextView(getContext());
+                cell.setText(cellValue);
+                cell.setPadding(8, 8, 8, 8);
+                cell.setBackgroundResource(R.drawable.cell_border);
+                row.addView(cell);
+            }
+            tableLayout.addView(row, position);
+            if (isRowNumberingEnabled) {
+                updateRowNumbers();
+            }
+        }
+    }
+
+    public void removeRow(int position) {
+        if (position >= 0 && position < tableLayout.getChildCount()) {
+            tableLayout.removeViewAt(position);
+            if (isRowNumberingEnabled) {
+                updateRowNumbers();
+            }
+        }
+    }
+
+    public void setCellText(int row, int column, String text) {
+        if (isValidPosition(row, column)) {
+            TableRow tableRow = (TableRow) tableLayout.getChildAt(row);
+            TextView cell = (TextView) tableRow.getChildAt(column);
+            cell.setText(text);
+        }
+    }
+
+    public void addCellToRow(int row, String cellValue) {
+        if (isValidRow(row)) {
+            TableRow tableRow = (TableRow) tableLayout.getChildAt(row);
             TextView cell = new TextView(getContext());
             cell.setText(cellValue);
             cell.setPadding(8, 8, 8, 8);
             cell.setBackgroundResource(R.drawable.cell_border);
-            row.addView(cell);
-        }
-        tableLayout.addView(row, position);
-        if (isRowNumberingEnabled) {
-            updateRowNumbers();
-        }
-    }
-    public void removeRow(int position) {
-        tableLayout.removeViewAt(position);
-        if (isRowNumberingEnabled) {
-            updateRowNumbers();
+            tableRow.addView(cell);
         }
     }
 
+    public void addHeaderRow(String[] headerValues) {
+        TableRow headerRow = new TableRow(getContext());
+        for (String headerValue : headerValues) {
+            TextView headerCell = new TextView(getContext());
+            headerCell.setText(headerValue);
+            headerCell.setPadding(8, 8, 8, 8);
+            headerCell.setBackgroundResource(R.drawable.header_cell_border);
+            headerCell.setTypeface(null, Typeface.BOLD);
+            headerRow.addView(headerCell);
+        }
+        tableLayout.addView(headerRow, 0);
+    }
+
+    public void setCellSpan(int row, int column, int rowSpan, int colSpan) {
+        if (isValidPosition(row, column)) {
+            TableRow tableRow = (TableRow) tableLayout.getChildAt(row);
+            TextView cell = (TextView) tableRow.getChildAt(column);
+            TableRow.LayoutParams params = new TableRow.LayoutParams();
+            params.span = colSpan;
+            params.height = rowSpan;
+            cell.setLayoutParams(params);
+        }
+    }
+
+    public void setCellClickListener(int row, int column, View.OnClickListener listener) {
+        if (isValidPosition(row, column)) {
+            TableRow tableRow = (TableRow) tableLayout.getChildAt(row);
+            TextView cell = (TextView) tableRow.getChildAt(column);
+            cell.setOnClickListener(listener);
+        }
+    }
+
+    public void addColumn(String[] columnValues) {
+        if (columnValues.length != tableLayout.getChildCount()) return;
+
+        for (int i = 0; i < tableLayout.getChildCount(); i++) {
+            TableRow tableRow = (TableRow) tableLayout.getChildAt(i);
+            TextView cell = new TextView(getContext());
+            cell.setText(columnValues[i]);
+            cell.setPadding(8, 8, 8, 8);
+            cell.setBackgroundResource(R.drawable.cell_border);
+            tableRow.addView(cell);
+        }
+    }
+
+    public void removeColumn(int column) {
+        for (int i = 0; i < tableLayout.getChildCount(); i++) {
+            TableRow tableRow = (TableRow) tableLayout.getChildAt(i);
+            if (column >= 0 && column < tableRow.getChildCount()) {
+                tableRow.removeViewAt(column);
+            }
+        }
+    }
+
+    public void setCellPadding(int row, int column, int left, int top, int right, int bottom) {
+        if (isValidPosition(row, column)) {
+            TableRow tableRow = (TableRow) tableLayout.getChildAt(row);
+            TextView cell = (TextView) tableRow.getChildAt(column);
+            cell.setPadding(left, top, right, bottom);
+        }
+    }
+
+    private boolean isValidPosition(int row, int column) {
+        return isValidRow(row) && isValidColumn(column, row);
+    }
+
+    private boolean isValidRow(int row) {
+        return row >= 0 && row < tableLayout.getChildCount();
+    }
+
+    private boolean isValidColumn(int column, int row) {
+        if (isValidRow(row)) {
+            TableRow tableRow = (TableRow) tableLayout.getChildAt(row);
+            return column >= 0 && column < tableRow.getChildCount();
+        }
+        return false;
+    }
+
+    private boolean isValidColumn(int column) {
+        for (int i = 0; i < tableLayout.getChildCount(); i++) {
+            TableRow tableRow = (TableRow) tableLayout.getChildAt(i);
+            if (column >= 0 && column < tableRow.getChildCount()) {
+                return true;
+            }
+        }
+        return false;
+    }
 }
